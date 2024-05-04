ffe779f7d8680de91041d8a56d69a53b993b333f
Song-aff <281218023@qq.com>
2024-05-04 13:10:48 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env,  process};
+
+use minigrep::{run, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod xx {
+
+    #[test]
+    fn one_one() {
+        assert_eq!(1, 1);
+    }
 }
